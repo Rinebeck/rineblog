46ae27da8cd012de5e1611db3cf3536ec1965488
Poncho@Starke <alfonso@wearestarke.com>
2021-07-14 11:35:12 -0500

    first commit

---
+++ .history/pages/post_20210714113424.js
@@ -0,0 +1,11 @@
+export default function Post() {
+    return (
+      <div class="post-container">
+        <h3>Here would be the post title</h3>
+
+        <article>
+            And here the article content
+        </article>
+      </div>
+    );
+}
